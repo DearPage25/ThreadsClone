2c36c25130956e766a11f6c7c7f8e93bde2b7be3
Odalmi <odalmip@icloud.com>
2026-06-12 16:44:58 -0400

    feat: first header design

--- ThreadsClone/ContentView.swift
+++ ThreadsClone/ContentView.swift
@@ -10,12 +10,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Button("Menu", systemImage: "water.waves", action: {})
+                    .labelStyle(.iconOnly)
+                Spacer()
+                Button("Home", systemImage: "at", action: {})
+                    .labelStyle(.iconOnly)
+                Spacer()
+                Button("Search", systemImage: "magnifyingglass", action: {})
+                    .labelStyle(.iconOnly)
+                
+            }
         }
         .padding()
+        Spacer()
     }
 }
 
