bfaed2bf4a1ecdf7e8e5eaa0da16efc9efbdafd8
Odalmi Rafel Paulino Peña <odalmip@Odalmis-MacBook-Pro.local>
2026-06-12 15:13:47 -0400

    Initial commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Odalmi Rafel Paulino Peña on 6/12/26.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
